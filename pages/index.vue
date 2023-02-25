37a3061b7ff15d172b25c59773b45923254a873d
Muhammad Fitrie <mfitrie78@kali.fitrie.com>
2023-02-25 16:22:32 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem qui explicabo ipsa, 
+            veniam id asperiores illo dolores ratione tempore, dolor, 
+            ab neque totam cupiditate voluptatum laborum dicta nihil? Voluptatem, mollitia.
+        </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem qui explicabo ipsa, 
+            veniam id asperiores illo dolores ratione tempore, dolor, 
+            ab neque totam cupiditate voluptatum laborum dicta nihil? Voluptatem, mollitia.
+        </p>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+    h2{
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p{
+        margin: 20px 0;
+    }
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, nulla illum placeat corporis nam aperiam eius quia nemo soluta quod.</p>
+    </div>
+</template>
+
+<script setup>
+    const {id} = useRoute().params;
+
+    definePageMeta({
+        layout: 'products',
+    });
+</script>
+
+<style lang="scss" scoped>
+
+</style>
